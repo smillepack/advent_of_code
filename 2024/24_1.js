35a649f24edd494fca064ec876bda380cab66380
Aliaksandr Kryvanosau <akryvanosau@avallain.com>
2024-12-25 18:08:40 +0300

    feat: 2024: 24s

---
+++ 2024/24_1.js
@@ -0,0 +1,59 @@
+import * as fs from "node:fs/promises";
+
+// const file = await fs.readFile("dummy_small.txt", { encoding: "utf8" });
+const file = await fs.readFile("dummy.txt", { encoding: "utf8" });
+const data = file.split("\n")
+// data.pop();
+
+console.log(data)
+
+const separator = data.indexOf('');
+
+const refsData = data.toSpliced(separator);
+const inputData = data.toSpliced(0, separator + 1);
+
+// console.log('refsData', refsData)
+// console.log('input', input)
+
+const refs = new Map();
+refsData.forEach((str) => {
+  const [key, value] = str.split(": ");
+
+  refs.set(key, parseInt(value));
+});
+console.log('refs', refs)
+
+const rules = [];
+inputData.forEach((str) => {
+  const [leftKey, command, rightKey,, result] = str.split(' ')
+
+  rules.push({ leftKey, command, rightKey, result });
+});
+
+// console.log('rules', rules)
+
+const commands = new Map([
+  ['AND', (l, r) => l & r],
+  ['OR', (l, r) => l | r],
+  ['XOR', (l, r) => l ^ r],
+]);
+
+
+while (rules.some(({ leftKey, rightKey, result }) => !refs.has(leftKey) || !refs.has(rightKey) || !refs.has(result))) {
+  rules.forEach(({ leftKey, command, rightKey, result }) => {
+    if (!refs.has(leftKey) || !refs.has(rightKey)) return;
+  
+    refs.set(result, commands.get(command)(refs.get(leftKey), refs.get(rightKey)));
+  });
+};
+console.log('refs', refs);
+
+let result = [];
+refs.forEach((_, key) => {
+  if (!key.startsWith('z')) return;
+
+  result.push(key)
+});
+result = result.sort().reverse().map((key) => refs.get(key)).join('');
+console.log('result', parseInt(result, 2));
+
---
+++ 2024/24_2.js
@@ -0,0 +1,84 @@
+import * as fs from "node:fs/promises";
+
+const file = await fs.readFile("dummy.txt", { encoding: "utf8" });
+const data = file.split("\n")
+const separator = data.indexOf('');
+
+const refsData = data.toSpliced(separator);
+const inputData = data.toSpliced(0, separator + 1);
+
+const refs = new Map();
+refsData.forEach((str) => {
+  const [key, value] = str.split(": ");
+
+  refs.set(key, parseInt(value));
+});
+
+const rules = [];
+inputData.forEach((str) => {
+  const [leftKey, command, rightKey,, result] = str.split(' ')
+
+  rules.push({ keys: [leftKey, rightKey], command, result });
+});
+
+const getRule = (x, y, c) => {
+  return rules.find(({ keys, command, result }) => {
+    if (keys.includes(x) && keys.includes(y) && command === c) {
+      return result;
+    } else {
+      return false;
+    }
+  });
+}
+
+const x00 = `x${('0' + 0).slice(-2)}`;
+const y00 = `y${('0' + 0).slice(-2)}`;
+
+// const z00 = getRule(x00, y00, 'XOR');
+let carry = getRule(x00, y00, 'AND');
+// console.log(x00, y00, z00, carry);
+
+const swapped = [];
+for (let i = 1; i <= 40; i++) {
+  // console.log(" ");
+  // console.log('swapped', swapped);
+
+  const xnn = `x${('0' + i).slice(-2)}`;
+  const ynn = `y${('0' + i).slice(-2)}`;  
+  const xXORy = getRule(xnn, ynn, 'XOR');
+  const xANDy = getRule(xnn, ynn, 'AND');
+
+  // console.log(xnn, ynn);
+  // console.log('xXORy', xXORy);
+  // console.log('xANDy', xANDy);
+
+  const z = getRule(carry.result, xXORy.result, 'XOR');
+  const prepCarry = getRule(carry.result, xXORy.result, 'AND');
+  // console.log('z', z);
+  // console.log('prepCarry', prepCarry);
+
+  if (z === undefined && prepCarry === undefined) {
+    [xXORy.result, xANDy.result] = [xANDy.result, xXORy.result];
+
+    i = 0;
+    carry = getRule(x00, y00, 'AND');
+    swapped.push(xXORy.result, xANDy.result);
+    continue;
+  }
+  
+  carry = getRule(prepCarry.result, xANDy.result, 'OR');
+  // console.log('carry', carry);
+
+  if (!z.result.startsWith('z')) {
+    const current = rules.find(({ result }) => result === z.result);
+    const expected = rules.find(({ result }) => result === `z${('0' + i).slice(-2)}`);
+
+    [current.result, expected.result] = [expected.result, current.result];
+
+    i = 0;
+    carry = getRule(x00, y00, 'AND');
+    swapped.push(current.result, expected.result);
+  }
+}
+
+console.log('swapped', swapped.toSorted().join(','));
